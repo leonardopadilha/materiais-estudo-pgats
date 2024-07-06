3eb3eea39a4f879a40b0f5e05eaf61883a2a18e2
leonardopadilha <leonardopadilha28@gmail.com>
2024-07-06 06:30:30 -0300

    Apagando registro via api

--- modulo05/automacao_api/primeiro-teste/web/tests/steps/user/cadastro.spec.js
+++ modulo05/automacao_api/primeiro-teste/web/tests/steps/user/cadastro.spec.js
@@ -1,6 +1,6 @@
 // @ts-check
 require('dotenv').config()
-const { test } = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 const { LoginPage } = require('../../pages/login.page');
 const { Menu } = require('../../Components/menu');
 const { CadastroPage } = require('../../pages/user/cadastro.page');
@@ -13,6 +13,8 @@ let menu;
 const LOGIN = process.env.LOGIN
 const SENHA = process.env.SENHA
 
+const URL_API = "http://localhost:3000/users"
+
 test.beforeEach(async ({ page }) => {
   loginPage = new LoginPage(page);
   cadastroPage = new CadastroPage(page);
@@ -20,17 +22,23 @@ test.beforeEach(async ({ page }) => {
   await loginPage.visit('/')
 })
 
-test('Deve cadastrar usuário com sucesso', async () => {
+test('Deve cadastrar usuário com sucesso', async ({ request }) => {
   const MENU_CADASTRO_USUARIOS = process.env.MENU_CADASTRO_USUARIOS
 
   const usuario = data.sucesso
 
   await loginPage.login(LOGIN, SENHA)
   await menu.selectCadastro(MENU_CADASTRO_USUARIOS)
+
   await cadastroPage.novoUsuario(usuario)
   await cadastroPage.clickOnCadastrar()
   await cadastroPage.cadastroSucesso('Usuário cadastrado com sucesso!')
   await cadastroPage.clickOk()
   await cadastroPage.verificarTabela(usuario.email)
+
+  const idUsuario = await request.get(URL_API)
+  const retorno = await idUsuario.json()
+  const deleteUsuario = await request.delete(`${URL_API}/${retorno[0].id}`)
+  expect(deleteUsuario.ok()).toBeTruthy()
 });
 
