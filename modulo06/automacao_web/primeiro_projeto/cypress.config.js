418d509c40e9ff653ffd0e2079e89afefb89dc2f
leonardopadilha <leonardopadilha28@gmail.com>
2024-08-04 09:46:16 -0300

    Incluindo novas configurações

--- modulo06/automacao_web/primeiro_projeto/cypress.config.js
+++ modulo06/automacao_web/primeiro_projeto/cypress.config.js
@@ -3,8 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.automationexercise.com',
+    watchForFileChanges: false,
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
