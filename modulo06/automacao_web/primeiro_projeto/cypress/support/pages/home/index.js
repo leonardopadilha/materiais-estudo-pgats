0f5a9247c4d014e62460296b28fcd6824dcbb21f
leonardopadilha <leonardopadilha28@gmail.com>
2024-08-19 05:13:11 -0300

    Adicionando métodos de inscrição e de confirmação da compra

--- modulo06/automacao_web/primeiro_projeto/cypress/support/pages/home/index.js
+++ modulo06/automacao_web/primeiro_projeto/cypress/support/pages/home/index.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 import linkMenu from '../../constants/constants';
 import { el } from '../../elements/elements'
+
 class HomePage {
     go(rota) {
         cy.visit(rota)
@@ -11,9 +12,40 @@ class HomePage {
             .should('be.visible')
     }
 
-    logged() {
+    logged(name) {
+        cy.get(el.logguedIn)
+            .last()
+            .should('be.visible')
+            .contains(name)
+    }
+
+    logOut() {
         cy.contains(linkMenu.LOGOUT)
             .should('be.visible')
+            .click()
+    }
+
+    subscription(email) {
+        cy.get(el.inputSubscription).scrollIntoView()
+            .type(email)
+
+        cy.get(el.btnSubscribe)
+            .click()
+    }
+
+    subscribed(msg) {
+        cy.get(el.msgSubscribed)
+            .should('have.text', msg)
+    }
+
+    addProducts() {
+        cy.get(el.addToCart)
+            .first()
+            .click()
+
+        cy.get(el.btnContinueShopping)
+            .should('be.visible')
+            .click()
     }
 }
 
